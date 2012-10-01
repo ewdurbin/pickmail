4cba010f56a83e5bc88682a3b288c0ffc8080051
Ernest W. Durbin III <ewdurbin@gmail.com>
2012-09-30 21:29:40 -0400

    configure server/port/user on cli

--- pickmail/parser.py
+++ pickmail/parser.py
@@ -5,10 +5,13 @@ class Parser:
 
     def __init__(self):
         self.parser = optparse.OptionParser()
-        self.parser.add_option('-b', '--batch-size', dest='batchsize', default=10, help='messages per batch')
+        self.parser.add_option('-s', '--server', dest='server', default=None, help='imap server')
+        self.parser.add_option('-p', '--port', dest='port', default=None, help='imap port')
+        self.parser.add_option('-u', '--user', dest='user', default=None, help='imap username')
         self.parser.add_option('-m', '--mailbox', dest='mailbox', default="INBOX", help='mailbox to query')
+        self.parser.add_option('-b', '--batch-size', dest='batchsize', default=10, help='messages per batch')
         self.parser.add_option('-c', '--config', dest='config', default=None, help='configuration file')
-        self.parser.add_option('-s', '--setup', action="store_true", dest='setup', help='setup ~/.pickmailrc configuration file')
+        self.parser.add_option('--setup', action="store_true", dest='setup', help='setup ~/.pickmailrc configuration file')
         self.parser.add_option('-w', '--wat', action="store_true", dest='password', help='save password in configuration file')
         self.options = None
         self.args = None
@@ -19,6 +22,12 @@ class Parser:
             configuration = pickmail.Config(config_file=self.options.config)
         if self.options.setup:
             configuration.setup_config(persist=True, store_pass=self.options.password)
+        if self.options.server:
+            configuration.server = self.options.server
+        if self.options.port:
+            configuration.port = self.options.port
+        if self.options.user:
+            configuration.username = self.options.user
         if self.options.batchsize:
             configuration.batchsize = self.options.batchsize
         if self.options.mailbox:
